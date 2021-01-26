cf1ca88cc8d5f858e58e182f80f8441de8284764
sebashack <sebastian0092@gmail.com>
2021-01-26 16:33:00 -0500

    Fix style

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,11 @@
 import sys
 import numpy as np
 
+
 def main(argv):
-    a = np.array([1,2,3,4])
+    a = np.array([1, 2, 3, 4])
     print(a)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])

eecc0ad7a26509569ba0eb55adeaeac801e27981
sebashack <sebastian0092@gmail.com>
2021-02-04 16:08:27 -0500

    Set up harcoded proof of concept

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,26 @@
 import sys
-import numpy as np
+import os
+
+from simulator import read_csv_data, gen_day_samples
 
 
 def main(argv):
-    a = np.array([1, 2, 3, 4])
-    print(a)
+    vehicle_csv_path = mk_csv_file_path("vehicle_list.csv")
+    coord_csv_path = mk_csv_file_path("coord_list.csv")
+    names_csv_path = mk_csv_file_path("person_list.csv")
+
+    csv_data = read_csv_data(vehicle_csv_path, coord_csv_path, names_csv_path)
+    # Run simulation for 30 days, a max number of 12 vehicles per day
+    # and a max number of 8 stops per vehicle per day
+    simulation_data = gen_day_samples(30, 12, 12, csv_data)
+
+    for day in simulation_data:
+        print(day)
+        print("\n")
+
+
+def mk_csv_file_path(path):
+    return str(os.path.join(os.getcwd(), path))
 
 
 if __name__ == "__main__":

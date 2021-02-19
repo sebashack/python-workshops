e6b9f5bd384411b4061f3d028282ef79fb651540
sebashack <sebastian0092@gmail.com>
2021-02-19 11:23:28 -0500

    Implement test for flattened sample

--- test/test_simulator.py
+++ test/test_simulator.py
@@ -2,6 +2,7 @@ import unittest
 
 from simulator import (
     gen_vehicle_day_samples,
+    flatten_vehicle_day_samples,
     read_csv_data,
 )
 from utils import mk_csv_file_path
@@ -25,6 +26,29 @@ class TestSimulator(unittest.TestCase):
         for vehicle_days in samples:
             drivers = list(map(lambda vd: vd.driver, vehicle_days))
             self.assertTrue(no_duplicates_check(drivers))
+
+    def test_gen_vehicle_day_samples_has_same_days_as_flattened_version(self):
+        vehicle_csv_path = mk_csv_file_path("vehicle_list.csv")
+        names_csv_path = mk_csv_file_path("person_list.csv")
+        csv_data = read_csv_data(vehicle_csv_path, names_csv_path)
+        medellin_center = (6.251404, -75.575261)
+        rad = 0.055
+
+        # Run simulation for 10 days, a max number of 10 vehicles per day
+        # and a max number of 10 stops per vehicle per day.
+        # This creates locations approximately in "El area Metropolitana".
+        # Medellin's center is located approximately at
+        # (lat = 6.251404, lon = -75.575261)
+        sample = gen_vehicle_day_samples(10, 10, 10, medellin_center, rad, csv_data)
+        sample_flattened = flatten_vehicle_day_samples(sample)
+
+        sample_elements = []
+        for vehicle_days in sample:
+            for v_day in vehicle_days:
+                sample_elements.append(v_day)
+
+        for (x, y) in zip(sample_elements, sample_flattened):
+            self.assertEqual(x, y)
 
 
 # Helpers

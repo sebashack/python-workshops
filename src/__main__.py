2e57099ecbdf7da48fefd1553aff40a0d7956d63
sebashack <sebastian0092@gmail.com>
2021-03-02 09:16:00 -0500

    Add minimum_edit_distance algorithm

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import sys
 
 from sorting import quick_sort, merge_sort, bubble_sort
+from edit_distance import minimum_edit_distance
 from utils import (
     plot_xs_vs_ys,
     gen_sort_method_data_points,
@@ -9,32 +10,34 @@ from utils import (
 
 
 def main(argv):
-    def generator(n):
-        return gen_random_integers_with_seed(n, 10)
+    # def generator(n):
+    #     return gen_random_integers_with_seed(n, 10)
 
-    bubble_sort_plot_data = gen_sort_method_data_points(
-        1000, 20, bubble_sort, "bubble_sort", generator
-    )
-    quick_sort_plot_data = gen_sort_method_data_points(
-        1000, 20, quick_sort, "quick_sort", generator
-    )
-    merge_sort_plot_data = gen_sort_method_data_points(
-        1000, 20, merge_sort, "merge_sort", generator
-    )
-    py_sort_plot_data = gen_sort_method_data_points(
-        1000, 20, lambda l: l.sort(), "py_sort", generator
-    )
+    # bubble_sort_plot_data = gen_sort_method_data_points(
+    #     1000, 20, bubble_sort, "bubble_sort", generator
+    # )
+    # quick_sort_plot_data = gen_sort_method_data_points(
+    #     1000, 20, quick_sort, "quick_sort", generator
+    # )
+    # merge_sort_plot_data = gen_sort_method_data_points(
+    #     1000, 20, merge_sort, "merge_sort", generator
+    # )
+    # py_sort_plot_data = gen_sort_method_data_points(
+    #     1000, 20, lambda l: l.sort(), "py_sort", generator
+    # )
 
-    plot_xs_vs_ys(
-        [
-            bubble_sort_plot_data,
-            merge_sort_plot_data,
-            quick_sort_plot_data,
-            py_sort_plot_data,
-        ],
-        "TIME",
-        "N",
-    )
+    # plot_xs_vs_ys(
+    #     [
+    #         bubble_sort_plot_data,
+    #         merge_sort_plot_data,
+    #         quick_sort_plot_data,
+    #         py_sort_plot_data,
+    #     ],
+    #     "TIME",
+    #     "N",
+    # )
+
+    print(minimum_edit_distance("ABC", "DEF"))
 
 
 if __name__ == "__main__":
---
+++ src/edit_distance.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def minimum_edit_distance(source, target):
+
+    s = "#" + source
+    t = "#" + target
+
+    solution = np.zeros((len(s), len(t)))
+
+    solution[0] = [j for j in range(len(t))]
+    solution[:, 0] = [j for j in range(len(s))]
+
+    for r in range(1, len(s)):
+        for c in range(1, len(t)):
+            if t[c] != s[r]:
+                solution[r, c] = min(solution[r-1, c], solution[r, c-1]) + 1
+            else:
+                solution[r, c] = solution[r-1, c-1]
+
+    return solution

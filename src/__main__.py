5d359a205354744b7223015382be3620eb564018
sebashack <sebastian0092@gmail.com>
2021-04-13 10:37:03 -0500

    Set up simple face detection

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,56 @@
 import sys
+import cv2
+
+
+def detect_faces(image):
+    face_cascade = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    face_classifier = cv2.CascadeClassifier(face_cascade)
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Rostros detectados imagenAnalizar
+    faces = face_classifier.detectMultiScale(
+        grey_image, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30)
+    )
+
+    face_images = []
+    for (x, y, w, h) in faces:
+        ROI = image[y : y + h, x : x + w]
+        face_images.append(ROI)
+
+    return (faces, face_images)
+
+
+def show_webcam(mirror=False):
+    video = cv2.VideoCapture(0)
+    while True:
+        check, frame = video.read()
+        if mirror:
+            frame = cv2.flip(frame, 1)
+
+        data_faces, face_images = detect_faces(frame)
+        modified_frame = add_rectangle(frame, data_faces)
+        # crearRostros(frame, data_faces)
+
+        cv2.imshow("mywebcam", modified_frame)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+def add_rectangle(frame, faces):
+    print("Face found in image")
+
+    color = (0, 255, 255)
+    thickness = 15
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, thickness)
+
+    return frame
+
 
 def main(argv):
-    print("hello")
+    show_webcam()
 
 
 if __name__ == "__main__":
